0c1473ed25df1ccedd5246ae0840d8842a25f578
jamalmuradagaev <jamalmuradagaev@yandex.ru>
2024-06-09 11:41:09 +0300

    настроена маршрутизация для каждого контакта, для поиска и нового контакта

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import Root, { loader, action } from './routes/root.jsx'
+import ErrorPage from './error-page.jsx'
+import Contact, { loader as contactLoader, action as contactAction } from './routes/contact.jsx'
+import EditContact, { action as editAction } from './routes/edit.jsx'
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader,
+    action,
+    children: [
+      {
+        index: true,
+        element: <Index />
+      },
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+        loader: contactLoader,
+        action: contactAction
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction
+      },
+      {
+        path: 'contacts/:contactId/destroy',
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      }
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
